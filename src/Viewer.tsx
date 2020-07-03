7989a65afa16e25ed9735e1bf875d526d28c3eee
daishi <daishi@axlight.com>
2020-07-03 22:09:48 +0900

    run format

--- src/Viewer.tsx
+++ src/Viewer.tsx
@@ -16,8 +16,11 @@ const Viewer: React.FC<Props> = ({ elements }) => {
       const canvas = canvasRef.current;
       const width = canvas.parentElement?.clientWidth || 300;
       const height = canvas.parentElement?.clientHeight || 300;
-      // https://github.com/GoogleChromeLabs/worker-plugin/issues/43
-      const worker = new Worker("./viewer.worker", { name: "x", type: "module" });
+      const worker = new Worker("./viewer.worker", {
+        // https://github.com/GoogleChromeLabs/worker-plugin/issues/43
+        name: "x",
+        type: "module",
+      });
       canvas.style.width = `${width}px`;
       canvas.style.height = `${height}px`;
       const offscreen = canvas.transferControlToOffscreen();
